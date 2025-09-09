ff22ee02088e6b790db5fd9cf23a421098413c35
Gabriel Santana <santanag311@gmail.com>
2025-09-09 11:57:37 -0300

    feat: create Logs model

---
+++ src/models/Logs.js
@@ -0,0 +1,24 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const Logs = sequelize.define("Logs", {
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Users',
+            key: 'id'
+        }
+    },
+    module: {
+        type: DataTypes.ENUM('Account', 'Schedule', 'Auth'),
+        allowNull: false
+    },
+    activityDate: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+    }
+})
+
+export default Logs;
